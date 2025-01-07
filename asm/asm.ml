7469ecbe869213d05f0f2f18fb03da6052070ccc
Aghilas Y. Boussaa <aghilas.boussaa@ens.psl.eu>
2025-01-07 13:21:06 +0100

    Compatibilité avec d'anciennes versions d'ocaml.

--- asm/asm.ml
+++ asm/asm.ml
@@ -1,3 +1,5 @@
+let (%) f g = fun x -> f (g x)
+
 let _ =
   let read_file f =
     let ic = open_in f in
@@ -5,6 +7,6 @@ let _ =
     let p = Parser.program Lexer.lexer lexbuf in
     close_in ic;
     let oc = open_out "out" in
-    List.iter (output_string oc) (List.map (Fun.compose Emit.int32 Encode.instruction ) p);
+    List.iter (output_string oc) (List.map (Emit.int32 % Encode.instruction) p);
     close_out oc
   in Arg.parse [] read_file ""
