a2fa966971d21477ffd334e8879c5cda3f9c47bb
Olivier Henry <olivier.henry@gmx.com>
2025-01-27 20:39:29 +0100

    Added a simulator to the assembler, added negative immediates to the lexer

--- asm/asm.ml
+++ asm/asm.ml
@@ -1,12 +1,25 @@
+let usage = "asm.exe [options] file.s"
+let simulator = ref false
+let print_step = ref false
+let spec = [
+  "--simulator", Arg.Set simulator, "  run the simulator, otherwise run the assembler";
+  "--verbose", Arg.Set print_step, "  when used with --simulator, dump registers on every non-jump step";
+]
+
 let (%) f g = fun x -> f (g x)
 
 let _ =
+  let p = ref [] in
   let read_file f =
     let ic = open_in f in
     let lexbuf = Lexing.from_channel ic in
-    let p = Parser.program Lexer.lexer lexbuf in
-    close_in ic;
+    p := Parser.program Lexer.lexer lexbuf;
+    close_in ic in
+  Arg.parse spec read_file "";
+  if !simulator then
+    Simulator.sim {Simulator.regs = Array.make 32 Int64.zero; Simulator.ram = Bytes.make 2048 '\000'; Simulator.print_step = !print_step} (Array.of_list !p) 0
+  else begin
     let oc = open_out "out" in
-    List.iter (output_string oc) (List.map (Emit.int32 % Encode.instruction) p);
+    List.iter (output_string oc) (List.map (Emit.int32 % Encode.instruction) !p);
     close_out oc
-  in Arg.parse [] read_file ""
+  end
---
+++ asm/simulator.ml
@@ -0,0 +1,73 @@
+open Syntax
+
+type env = {ram : Bytes.t; regs : Int64.t array; print_step : bool}
+
+let rec sim env prog i =
+  let nojmp = ref true in
+  begin match prog.(i) with
+  | Op (op, r2, r1, dest) ->
+    let res = begin match op with
+      | Or -> Int64.logor env.regs.(r1) env.regs.(r2)
+      | And -> Int64.logand env.regs.(r1) env.regs.(r2)
+      | Add -> Int64.add env.regs.(r1) env.regs.(r2)
+      | Sub -> Int64.sub env.regs.(r1) env.regs.(r2)
+      | Sll -> Int64.shift_left env.regs.(r1) (Int64.to_int env.regs.(r2))
+      | Slt -> if Int64.compare env.regs.(r1) env.regs.(r2) < 0 then Int64.one else Int64.zero
+      | Sltu -> if Int64.unsigned_compare env.regs.(r1) env.regs.(r2) < 0 then Int64.one else Int64.zero
+      | Xor -> Int64.logxor env.regs.(r1) env.regs.(r2)
+      | Srl -> Int64.shift_right_logical env.regs.(r1) (Int64.to_int env.regs.(r2))
+      | Sra -> Int64.shift_right env.regs.(r1) (Int64.to_int env.regs.(r2))
+    end in
+    env.regs.(dest) <- res
+  | Opi (op, r1, imm, dest) ->
+    let res = begin match op with
+      | Or -> Int64.logor env.regs.(r1) (Int64.of_int imm)
+      | And -> Int64.logand env.regs.(r1) (Int64.of_int imm)
+      | Add -> Int64.add env.regs.(r1) (Int64.of_int imm)
+      | Sub -> Int64.sub env.regs.(r1) (Int64.of_int imm)
+      | Sll -> Int64.shift_left env.regs.(r1) imm
+      | Slt -> if Int64.compare env.regs.(r1) (Int64.of_int imm) < 0 then Int64.one else Int64.zero
+      | Sltu -> if Int64.unsigned_compare env.regs.(r1) (Int64.of_int imm) < 0 then Int64.one else Int64.zero
+      | Xor -> Int64.logxor env.regs.(r1) (Int64.of_int imm)
+      | Srl -> Int64.shift_right_logical env.regs.(r1) imm
+      | Sra -> Int64.shift_right env.regs.(r1) imm
+    end in
+    env.regs.(dest) <- res
+  | Load (_, offset, r2, r1) ->
+    let addr = (Int64.to_int env.regs.(r2)) + offset in
+    env.regs.(r1) <- Bytes.get_int64_le env.ram addr
+  | Store (_, offset, r2, r1) ->
+    let addr = (Int64.to_int env.regs.(r2)) + offset in
+    Bytes.set_int64_le env.ram addr env.regs.(r1)
+  | Branch (bop, r1, r2, (_, addenvs)) ->
+    let v1 = env.regs.(r1) and v2 = env.regs.(r2) in
+    if match bop with
+      | Eq -> v1 = v2
+      | Ne -> v1 <> v2
+      | Lt -> v1 < v2
+      | Ge -> v1 >= v2
+      | Ltu -> Int64.unsigned_compare v1 v2 < 0
+      | Geu -> Int64.unsigned_compare v1 v2 >= 0
+    then begin
+      nojmp := false;
+      sim env prog (!addenvs / 4)
+    end
+  | Jal (r, (_, addr)) ->
+    env.regs.(r) <- Int64.of_int (4*i);
+    nojmp := false;
+    sim env prog (!addr / 4)
+  | Nop -> ()
+  | _ -> failwith "pas trop implémenté"
+  end;
+  if !nojmp then begin
+    if env.print_step || i+1 = Array.length prog then begin
+      print_endline "\nregister | value";
+      print_endline "---------|------";
+      Array.iteri (fun i n ->
+        let s = (Int64.to_string n) in
+        Printf.printf "x%-7d | %s\n" i s
+    ) env.regs
+    end;
+    if i+1 < Array.length prog then sim env prog (i+1)
+  end
+
