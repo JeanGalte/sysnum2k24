5962696b27315f1ef757baef452e1f4de07db435
Olivier Henry <olivier.henry@gmx.com>
2025-01-27 21:20:24 +0100

    Fixed store instructions in the simulator.

--- asm/simulator.ml
+++ asm/simulator.ml
@@ -3,7 +3,7 @@ open Syntax
 type env = {ram : Bytes.t; regs : Int64.t array; print_step : bool}
 
 let rec sim env prog i =
-  let nojmp = ref true in
+  let nojmp = ref true and noprint = ref false in
   begin match prog.(i) with
   | Op (op, r2, r1, dest) ->
     let res = begin match op with
@@ -36,10 +36,14 @@ let rec sim env prog i =
   | Load (_, offset, r2, r1) ->
     let addr = (Int64.to_int env.regs.(r2)) + offset in
     env.regs.(r1) <- Bytes.get_int64_le env.ram addr
-  | Store (_, offset, r2, r1) ->
+  | Store (_, r1, offset, r2) ->
     let addr = (Int64.to_int env.regs.(r2)) + offset in
-    Bytes.set_int64_le env.ram addr env.regs.(r1)
+    Bytes.set_int64_le env.ram addr env.regs.(r1);
+    print_endline (string_of_int r1);
+    print_endline (Int64.to_string env.regs.(r1));
+    print_endline (Int64.to_string (Bytes.get_int64_le env.ram addr))
   | Branch (bop, r1, r2, (_, addenvs)) ->
+    noprint := true;
     let v1 = env.regs.(r1) and v2 = env.regs.(r2) in
     if match bop with
       | Eq -> v1 = v2
@@ -53,21 +57,20 @@ let rec sim env prog i =
       sim env prog (!addenvs / 4)
     end
   | Jal (r, (_, addr)) ->
+    noprint := true;
     env.regs.(r) <- Int64.of_int (4*i);
     nojmp := false;
     sim env prog (!addr / 4)
-  | Nop -> ()
+  | Nop -> noprint := true
   | _ -> failwith "pas trop implémenté"
   end;
-  if !nojmp then begin
-    if env.print_step || i+1 = Array.length prog then begin
-      print_endline "\nregister | value";
-      print_endline "---------|------";
-      Array.iteri (fun i n ->
-        let s = (Int64.to_string n) in
-        Printf.printf "x%-7d | %s\n" i s
-    ) env.regs
-    end;
-    if i+1 < Array.length prog then sim env prog (i+1)
-  end
+  if not !noprint && (env.print_step || i+1 = Array.length prog) then begin
+    print_endline "\nregister | value";
+    print_endline "---------|------";
+    Array.iteri (fun i n ->
+      let s = (Int64.to_string n) in
+      Printf.printf "x%-7d | %s\n" i s
+  ) env.regs
+  end;
+  if !nojmp && i+1 < Array.length prog then sim env prog (i+1)
 
