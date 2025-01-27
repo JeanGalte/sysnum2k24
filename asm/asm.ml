317b09023da2994b4146537adc63dfd30983a665
Olivier Henry <olivier.henry@gmx.com>
2025-01-27 20:57:13 +0100

    Added options to the simulator and the assembler.

--- asm/asm.ml
+++ asm/asm.ml
@@ -1,9 +1,13 @@
 let usage = "asm.exe [options] file.s"
+let out_file = ref "out"
 let simulator = ref false
 let print_step = ref false
+let ram_size = ref 2048
 let spec = [
-  "--simulator", Arg.Set simulator, "  run the simulator, otherwise run the assembler";
-  "--verbose", Arg.Set print_step, "  when used with --simulator, dump registers on every non-jump step";
+  "-o", Arg.Set_string out_file, "  output file name";
+  "-simulator", Arg.Set simulator, "  run the simulator, otherwise run the assembler";
+  "-verbose", Arg.Set print_step, "  when used with --simulator, dump registers on every non-jump step";
+  "-ram", Arg.Set_int ram_size, "  when used with --simulator, sets the number of bytes in the simulated RAM (default 2048)"
 ]
 
 let (%) f g = fun x -> f (g x)
@@ -15,11 +19,12 @@ let _ =
     let lexbuf = Lexing.from_channel ic in
     p := Parser.program Lexer.lexer lexbuf;
     close_in ic in
-  Arg.parse spec read_file "";
+  Arg.parse spec read_file usage;
+  if List.length !p = 0 then Arg.usage spec usage;
   if !simulator then
-    Simulator.sim {Simulator.regs = Array.make 32 Int64.zero; Simulator.ram = Bytes.make 2048 '\000'; Simulator.print_step = !print_step} (Array.of_list !p) 0
+    Simulator.sim {Simulator.regs = Array.make 32 Int64.zero; Simulator.ram = Bytes.make !ram_size '\000'; Simulator.print_step = !print_step} (Array.of_list !p) 0
   else begin
-    let oc = open_out "out" in
+    let oc = open_out !out_file in
     List.iter (output_string oc) (List.map (Emit.int32 % Encode.instruction) !p);
     close_out oc
   end
